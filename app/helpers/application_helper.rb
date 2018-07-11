0c05204b4c80e66da3246b6036ec38b7630660c3
shinjaehun <gunsmoke.shin@gmail.com>
2018-07-11 17:21:47 +0000

    gravatar 프로필 이미지 추가

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def avatar_url(user)
+    # user email에서 gravatar_id를 추출하고 gravatar.com에서 해당하는 이미지를 불러옴 
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}.png?d=retro&s=150"
+  end
 end
