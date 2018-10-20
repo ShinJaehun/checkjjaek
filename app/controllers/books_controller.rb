112ba887d51657eae07a108263e668ffd36c7cc9
shinjaehun <gunsmoke.shin@gmail.com>
2018-10-20 12:43:21 +0000

    book과 post, 동시에 create 성공!

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -83,72 +83,44 @@ class BooksController < ApplicationController
   end
   
   def new
-    # book_search에서 new action을 호출해서
-    # @book을 생성하는 거라면 아래 실행
-    # 다른 곳에서 new action을 호출해서
-    # object를 생성할 때는 else로...
-      
+
     @book = Book.new
     @book.posts.new    
-    
-    # unless @book = Book.find_by(isbn: params[:isbn])
-    #   # book instance를 DB에 저장
-    #   @book = Book.create(
-    #     # 책 제목에 &가 있으면 &amp;가 붙어 저장되는 버그를 해결 
-    #     # title: params[:title].gsub(/&amp;/, "&"),
-    #     # 그냥 이렇게 라이브러리를 사용하는게 낫겠지!
-    #     title: CGI.unescapeHTML(params[:title]),
-    #     isbn:  params[:isbn],
-    #     authors:  params[:authors],
-    #     thumbnail:  thumbnail_path,
-    #     publisher: params[:publisher],
-    #     contents: CGI.unescapeHTML(params[:contents]),
-    #     url: params[:url],
-    #     date_time: params[:date_time],
-    #     translators: params[:translators],
-    #     category: params[:category]
-    #   )
 
-    # end
-  
-
-      # thumbnail_url = params[:thumbnail]
-      
-      # unless thumbnail_url.to_s.empty?
-      #   thumbnail_path = URI.unescape(thumbnail_url.match(/^http.+?(http.+?)%3F/)[1].to_s)
-      # else
-      #   thumbnail_path = nil  
-      # end
-      
   end
   
   def create
-
-    @book = Book.new(book_params)
-    @book.save
-
-    # unless @book = Book.find_by(isbn: @book.isbn)
-    # # unless @book = Book.find_by(isbn: params[:isbn])
-    #   puts "없어요"
-    #   puts "isbn : " + @book.isbn
-    # else
-      # puts "있어요"
-      
-      # params.require(:post)
-      # puts "isbn : " + @book.isbn
-      # puts "content 1 : " + params[:content].to_s
-      # puts "content 2 : " + @book.posts.content
-      
-      # # post = Post.new(postable: @book)
-      # post = @book.posts.new
-      # post.user = current_user
-      # # post.content = params[:content]
-      # # post.content = @book.attribute
-      # post.save
-      # # post.save
-    # end
-
     
+    unless @book = Book.find_by(isbn: book_params[:isbn])
+
+      thumbnail_url = book_params[:thumbnail]
+      
+      unless thumbnail_url.to_s.empty?
+        thumbnail_path = URI.unescape(thumbnail_url.match(/^http.+?(http.+?)%3F/)[1].to_s)
+      else
+        thumbnail_path = nil  
+      end
+  
+      @book = Book.create(
+        title: CGI.unescapeHTML(book_params[:title]),
+        isbn:  book_params[:isbn],
+        authors:  book_params[:authors],
+        thumbnail:  thumbnail_path,
+        publisher: book_params[:publisher],
+        contents: CGI.unescapeHTML(book_params[:contents]),
+        url: book_params[:url],
+        date_time: book_params[:date_time],
+        translators: book_params[:translators],
+        category: book_params[:category]
+      )
+      
+    end
+
+    # post = Post.new(content: book_params[:posts_attributes]['0'][:content])
+    post = @book.posts.new(content: book_params[:posts_attributes]['0'][:content])
+    post.user_id = book_params[:posts_attributes]['0'][:user_id]
+    post.save      
+
     redirect_to root_path
     
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,141 +46,6 @@ class PostsController < ApplicationController
     
   end
 
-  # def book_search
-  #   # keyword_book으로 검색한 경우(책 검색 결과에 포스팅하기)
-  #   if params.has_key?(:keyword_book)
-  #     @keyword_book = params[:keyword_book]
-      
-  #     # 검색어가 있다면...
-  #     if @keyword_book.present?
-
-  #       @size = 50 # 한 화면에 표시할 검색 결과의 수
-
-  #       if params[:page].to_s.empty?
-  #         # 시작 위치가 정해져 있지 않으면 기본적으로 첫 페이지 보여주기
-  #         @current_page = 1
-  #       else
-          
-  #         # 시작 위치가 설정되어 있으면 해당 페이지로
-  #         @current_page = params[:page].to_i
-
-  #       end
-        
-  #       url = "https://dapi.kakao.com/v2/search/book?query=" + @keyword_book + "&size=" + @size.to_s + "&page=" + @current_page.to_s 
-  #       # url = "https://dapi.kakao.com/v2/search/book?target=title&query=" + @keyword_book + "&size=" + @size.to_s + "&page=" + @current_page.to_s 
-        
-  #       uri = URI.encode(url)
-  #       res = RestClient.get(uri, headers={
-  #         'Authorization' => Rails.application.credentials.kakao[:authorization_key]})
-          
-  #       unitokor = eval(res)
-          
-  #       # puts unitokor
-          
-  #       json_g = JSON.generate(unitokor)
-  #       hash = JSON.parse(json_g)
-          
-  #       # @items = hash['items']
-  #       @total_count = hash['meta']['total_count']
-        
-  #       # puts "############################################################"
-  #       # puts @total_count
-  #       # puts "현재 페이지 : " + @current_page.to_s + " 출력 건수 : " + @size.to_s + "  page * size : " + (@current_page * @size).to_s 
-        
-  #       # 마지막 페이지
-  #       @max_index = @total_count / @size + 1
-        
-  #       # start_index와 end_index 값 지정하기
-  #       if @current_page > 2
-  #         @start_index = @current_page - 2
-  #         if @current_page <= @max_index - 2
-  #           @end_index = @current_page + 2 
-  #         else
-  #           @end_index = @max_index
-  #         end
-  #       else
-  #         @start_index = 1
-  #         if @max_index <= 5
-  #           @end_index = @max_index
-  #         else
-  #           @end_index = 5
-  #         end
-  #       end
-        
-  #       # puts "현재 페이지 : " + @current_page.to_s + " 마지막 페이지 : " + @max_index.to_s
-  #       # puts "start_index : " + @start_index.to_s + " end_index : " + @end_index.to_s 
-        
-  #       @items = hash['documents']
-
-  #     end
-  #   end
-  
-  #   # 검색어가 없으면 items에 빈 array 넘기기
-  #   @items ||= []
-    
-  # end
-
-
-  # GET /posts/new
-  # def new
-    
-  #   if params[:type] == "Book"
-  #     # isbn을 기준으로 books 테이블에 동일한 책이 없다면
-  #     unless @book = Book.find_by(isbn: params[:isbn])
-      
-  #       # image 같은 경우는 URL을 저장해야 하기 때문에...
-  #       thumbnail_url = params[:thumbnail]
-        
-  #       #image_path = URI.parse(url).host + URI.parse(params[:image]).path
-  #       # 이미지 경로 : bookthumb-phinf.pstatic.net/cover/001/559/00155992.jpg
-  #       #image_path = url.gsub(/\?.*/, '')
-  #       # 이미지 경로 : https://bookthumb-phinf.pstatic.net/cover/001/559/00155992.jpg
-  #       # 준우 샘이 경로는 이렇게 full URL 형식으로 놔두는 편이 낫다고...
-  #       # API가 제공하는 책 썸네일은 https인데 불러오지 못함...(인증서 관련)
-  #       # 그래서 경로 앞에 http://만 붙이도록 함
-  #       unless thumbnail_url.to_s.empty?
-  #         # image_path = "http://" + URI.parse(url).host + URI.parse(params[:image]).path
-  #         # thumbnail_path = URI.unescape(url.match(/http%.+/).to_s)
-          
-  #         # thumbnail_path = URI.unescape(thumbnail_url.match(/(http.*)(http.*)/)[2].to_s)
-  #         thumbnail_path = URI.unescape(thumbnail_url.match(/^http.+?(http.+?)%3F/)[1].to_s)
-  #       else
-  #         thumbnail_path = nil  
-  #       end
-        
-  #       # book instance를 DB에 저장
-  #       @book = Book.create(
-  #         # 책 제목에 &가 있으면 &amp;가 붙어 저장되는 버그를 해결 
-  #         # title: params[:title].gsub(/&amp;/, "&"),
-  #         # 그냥 이렇게 라이브러리를 사용하는게 낫겠지!
-  #         title: CGI.unescapeHTML(params[:title]),
-  #         isbn:  params[:isbn],
-  #         authors:  params[:authors],
-  #         thumbnail:  thumbnail_path,
-  #         publisher: params[:publisher],
-  #         contents: CGI.unescapeHTML(params[:contents]),
-  #         url: params[:url],
-  #         date_time: params[:date_time],
-  #         translators: params[:translators],
-  #         category: params[:category]
-  #       )  
-  #     end
-      
-  #     # 준우 샘이 위 로직은 find_or_create_by() 를 이용해서 구현하는 것도 좋을 것 같다고 하심
-      
-  #     # 새로 생성하든 기존의 인스턴스를 가지고 오든 @book 인스턴스를 가지고 새로운 post 인스턴스를 생성하기 위해
-  #     # 바인딩할 빈 @post 넘기기
-  #     # @post = Post.new(book: @book)
-  #     # @post = @postable.posts.new
-      
-  #     # 관련 책짹 보여주기 위해
-  #     @posts = Post.where(book_id: @book.id).order(created_at: :desc)
-
-  #   end
-    
-  #   # @post = @postable.posts.new
-  # end
-
   # GET /posts/1/edit
   def edit
     # and return은 조건에 해당한다면 이후의 코드를 건너뛰고 return 처리한다는 의미
@@ -201,22 +66,22 @@ class PostsController < ApplicationController
   
   # POST /posts
   # POST /posts.json
-  def create
-    @post = @postable.posts.new(post_params)
-    # 어느 순간에 postable이 정상적으로 작동되었는지 도저히 알 수가 없다...
-    @post.user = current_user
+  # def create
+  #   @post = @postable.posts.new(post_params)
+  #   # 어느 순간에 postable이 정상적으로 작동되었는지 도저히 알 수가 없다...
+  #   @post.user = current_user
 
-    respond_to do |format|
-      if @post.save
-        # format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.html { redirect_to root_path, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  #   respond_to do |format|
+  #     if @post.save
+  #       # format.html { redirect_to @post, notice: 'Post was successfully created.' }
+  #       format.html { redirect_to root_path, notice: 'Post was successfully created.' }
+  #       format.json { render :show, status: :created, location: @post }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @post.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
